ba419851eb9c475588ec198a3f237eece8663fe0
Kalyan <kalyan.allampalli@gmail.com>
2016-08-09 18:15:58 -0500

    adding line items to PR on click

--- app/purchase_requisition/actions/index.js
+++ app/purchase_requisition/actions/index.js
@@ -2,6 +2,6 @@ import { PR } from '../../../config/constants'
 
 export const addLineItem = () => {
   return (dispatch) => {
-    dispatch({ type: PR.ADD_PR_LINE_ITEM })
+    dispatch({ type: PR.ADD_PR_LINE_ITEM, data: { vendor: 'Vendor 1', business_unit: 'G1 P2P', currency: 'USD' } })
   }
 }
--- app/purchase_requisition/reducers/index.js
+++ app/purchase_requisition/reducers/index.js
@@ -1,12 +1,28 @@
 import { PR } from '../../../config/constants'
 import initialState from '../initial_state'
 
+// React's update() immutability helper.
+import update from 'react/lib/update'
+
+const LineItem = (state = {}, action) => {
+  const data = action.data
+  switch (action.type) {
+    case PR.ADD_PR_LINE_ITEM:
+      return {
+        vendor: data.vendor, business_unit: data.business_unit,
+        plant: data.plant, currency: data.currency
+      }
+    default:
+      state
+  }
+}
+
 const PurchaseRequisition = (state = initialState, action) => {
   switch (action.type) {
     case PR.ADD_PURCHASE_REQUISITON:
       return state
     case PR.ADD_PR_LINE_ITEM:
-      return Object.assign({}, state, {})
+      return update(state, { line_items: { $push: [LineItem(undefined, action)] } })
     default:
       return state
   }
