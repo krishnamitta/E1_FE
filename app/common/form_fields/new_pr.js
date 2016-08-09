bab0c3dd829bce6a860e0c623388e677c17be2f4
Kalyan <kalyan.allampalli@gmail.com>
2016-08-09 11:42:51 -0500

    new pr further fields added

--- app/common/form_fields/new_pr.js
+++ app/common/form_fields/new_pr.js
@@ -11,5 +11,26 @@ export default {
     type: 'dropdown',
     dataSource: [{ code: '1', description: 'Vendor 1' }, { code: '2', description: 'Vendor 2' }],
     required: true
+  },
+  business_unit: {
+    name: 'business_unit',
+    floatingLabel: 'Business Unit',
+    type: 'dropdown',
+    dataSource: [{ code: 'p2p', description: 'G1 P2P' }],
+    required: true
+  },
+  plant: {
+    name: 'plant',
+    floatingLabel: 'Plant',
+    type: 'dropdown',
+    dataSource: [{ code: '5001', description: 'Dallas' }, { code: '5002', description: 'Miami' }],
+    required: true
+  },
+  currency: {
+    name: 'currency',
+    floatingLabel: 'Currency',
+    type: 'dropdown',
+    dataSource: [{ code: 'usd', description: 'USD' }, { code: 'eur', description: 'EUR' }, { code: 'inr', description: 'INR' }],
+    required: true
   }
 }
