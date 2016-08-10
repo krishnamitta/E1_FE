2bfdafaefd291c842824f254e6edbf7bf4e4aac1
Vaishali Shah <vaishali.shah@copart.com>
2016-08-10 11:22:07 -0500

    Lineitem (#2)
    * added line item
    
    * some refactoring

---
+++ app/purchase_requisition/line_item/components/new_line_item_component.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import InputField from '../../../common/input_field'
+import newLineItem from '../../../common/form_fields/new_line_item'
+import Paper from 'material-ui/Paper'
+
+const LineItemComponent = (props) => {
+  return (
+    <div className="col-1-1">
+      <Paper>
+        <div className="col-1-5"><InputField attrs={ newLineItem.title } data={ props.data.title } /></div>
+        <div className="col-1-5"><InputField attrs={ newLineItem.vendor } data={ props.data.currency } /></div>
+        <div className="col-1-5"><InputField attrs={ newLineItem.business_unit } data={ props.data.business_unit } /></div>
+        <div className="col-1-5"><InputField attrs={ newLineItem.plant } data={ props.data.plant } /></div>
+        <div className="col-1-5"><InputField attrs={ newLineItem.currency } data={ props.data.currency } /></div>
+      </Paper>
+    </div>
+  )
+}
+
+LineItemComponent.propTypes = {
+  data: React.PropTypes.object,
+  items: React.PropTypes.array
+}
+
+export default LineItemComponent
