1b64b336fe06a683075cfb421569086522afdc75
Kalyan <kalyan.allampalli@gmail.com>
2016-08-22 13:29:28 -0500

    minor changes - icon

--- app/dashboard/components/index.js
+++ app/dashboard/components/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Tile from '../../common/tile'
 import { Link } from 'react-router'
-import ActionNoteAdd from 'material-ui/svg-icons/action/note-add'
 import ContentAddCircle from 'material-ui/svg-icons/content/add-circle'
 
 import styles from '../../styles'
@@ -11,7 +10,7 @@ const DashboardComponent = () => {
     <div className="col-1-1">
       <div className="col-1-5">
         <Link to="/purchase_requisitions/new">
-          <Tile label="Purchase Requisition" icon={ <ActionNoteAdd color="#FFFFFF" style={ styles.addIcon } /> } />
+          <Tile label="Purchase Requisition" icon={ <ContentAddCircle color="#FFFFFF" style={ styles.addIcon } /> } />
         </Link>
       </div>
       <div className="col-1-5">
