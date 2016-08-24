19deeec7fc87e88f161f149e691864342daccdf4
Kalyan <kalyan.allampalli@gmail.com>
2016-08-24 14:14:50 -0500

    dev environment url changes

--- config/environments/client/development.js
+++ config/environments/client/development.js
@@ -5,7 +5,7 @@ module.exports = {
       vendorList: "/VendorMaster?acctgroup='ZPUL'&sap-client=300&$format=json",
       countries: "/CountryMaster?locale='E'&sap-client=300&$format=json",
       UOMList: "/UomMaster?locale='E'&sap-client=300&$format=json",
-      plantList: "/PlantMaster?sap-client=300?locale='E'&$format=json",
+      plantList: "/PlantMaster?sap-client=300&locale='E'&$format=json",
       currencyList: "/CurrencyMaster?locale='E'&sap-client=300&$format=json",
       materialList: "/MaterialMaster?locale='E'&materialtype='ZAWA'&sap-client=300&$format=json",
       materialGroupList: "/MatGrpMaster?sap-client=300&locale='E'&$format=json",
